56115193c48a411a559b66d25698c68d5a7e9c60
Lee Presswood <pressedlee123@gmail.com>
2014-08-24 11:26:52 -0500

    Added initial project

---
+++ android/src/com/leepresswood/ants/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.leepresswood.ants.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.leepresswood.ants.Ants;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Ants(), config);
+	}
+}
