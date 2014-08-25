f4691004997ba7328b87f5a049d82aa4908925e5
Lee Presswood <pressedlee123@gmail.com>
2014-08-25 17:48:59 -0500

    Fixed linking problem.

--- android/src/com/leepresswood/ants/android/AndroidLauncher.java
+++ android/src/com/leepresswood/ants/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.leepresswood.ants.Ants;
+import com.leepresswood.antsoldiers.GameAntSoldiers;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Ants(), config);
+		initialize(new GameAntSoldiers(), config);
 	}
 }
