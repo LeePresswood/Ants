0a7c5d35142d108b5f0ea826d18d6a6612c5c85e
Lee Presswood <pressedlee123@gmail.com>
2014-08-25 19:44:01 -0500

    Added ScreenHolder to screen.

--- core/src/com/leepresswood/antsoldiers/GameAntSoldiers.java
+++ core/src/com/leepresswood/antsoldiers/GameAntSoldiers.java
@@ -1,27 +1,32 @@
 package com.leepresswood.antsoldiers;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.leepresswood.antsoldiers.holders.ScreenHolder;
+import com.leepresswood.antsoldiers.screens.ScreenGame;
 
-public class GameAntSoldiers extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class GameAntSoldiers extends Game
+{
+	@Override
+	public void create()
+	{
+		this.setScreen(new ScreenGame(new ScreenHolder()));
+	}	
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void render()
+	{
+		super.render();	
 	}
-
+	
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void dispose()
+	{
+		super.dispose();
+	}
+	
+	@Override
+	public void resume()
+	{
+		super.resume();	
 	}
 }
--- core/src/com/leepresswood/antsoldiers/screens/ScreenGame.java
+++ core/src/com/leepresswood/antsoldiers/screens/ScreenGame.java
@@ -3,9 +3,20 @@ package com.leepresswood.antsoldiers.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
+import com.leepresswood.antsoldiers.holders.ScreenHolder;
 
 public class ScreenGame extends ScreenAdapter
 {
+	private Vector2 origin_point;
+	private Vector2 screen_size;
+	
+	public ScreenGame(ScreenHolder screen_holder)
+	{
+		origin_point = new Vector2(screen_holder.x, screen_holder.y);
+		screen_size = new Vector2(screen_holder.width, screen_holder.height);
+	}
+
 	@Override
 	public void render(float delta)
 	{
