6af67fe3cce90d9ed42c41f442134601cce30fbc
Lee Presswood <pressedlee123@gmail.com>
2014-08-25 19:39:45 -0500

    Finished screen positioning.

--- core/src/com/leepresswood/antsoldiers/holders/ScreenHolder.java
+++ core/src/com/leepresswood/antsoldiers/holders/ScreenHolder.java
@@ -8,12 +8,29 @@ public class ScreenHolder
 	//Go for a 16:9 aspect ratio.
 	private float aspect_ratio = 16f / 9f;
 	
+	//Positioning.
+	public float x;
+	public float y;
 	public float width;
 	public float height;
 	
 	public ScreenHolder()
 	{
-		//Use the height of the device to find the width
+		//Use the height of the device to find the width.
 		height = Gdx.graphics.getHeight();
+		
+		//The width should be the height / aspect_ratio.
+		width = height / aspect_ratio;
+		
+		//If the above width is greater than the screen's width, use the screen's width to determine the height instead.
+		if(width >= Gdx.graphics.getWidth())
+		{
+			width = Gdx.graphics.getWidth();
+			height = width * aspect_ratio;
+		}
+		
+		//Determine where the x and y locations are by using the width and height.
+		x = Gdx.graphics.getWidth() / 2f - width / 2f;
+		y = Gdx.graphics.getHeight() / 2f - height / 2f;
 	}
 }
