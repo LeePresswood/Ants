f301a975087769886ca4c905eacd994d701b87d5
Lee Presswood <pressedlee123@gmail.com>
2014-10-17 10:06:35 -0500

    Removed bad class. Changed background color.

--- core/src/com/leepresswood/antsoldiers/screens/ScreenGame.java
+++ core/src/com/leepresswood/antsoldiers/screens/ScreenGame.java
@@ -2,6 +2,7 @@ package com.leepresswood.antsoldiers.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 import com.leepresswood.antsoldiers.holders.GUIHolder;
@@ -32,8 +33,8 @@ public class ScreenGame extends ScreenAdapter
 		update(delta);
 		
 		//Draw		
-		//Clear screen with black
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		//Clear screen with dark gray
+		Gdx.gl.glClearColor(40, 40, 40, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		gui_holder.render();
