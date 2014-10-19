d3a64ac22f274cd7bb8a01e352bb4370755c4924
Lee Presswood <pressedlee123@gmail.com>
2014-10-19 17:44:39 -0500

    Changed LevelParser.

--- core/src/com/leepresswood/antsoldiers/holders/GameHolder.java
+++ core/src/com/leepresswood/antsoldiers/holders/GameHolder.java
@@ -1,26 +1,32 @@
 package com.leepresswood.antsoldiers.holders;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
 import com.leepresswood.antsoldiers.gamegrid.GameGrid;
-import com.leepresswood.antsoldiers.gamegrid.tiles.GridBlock;
 import com.leepresswood.antsoldiers.screens.ScreenGame;
 
 public class GameHolder extends Holder
 {	
 	private OrthographicCamera camera;
 	private final int CAMERA_WIDTH = 800;
-	private final int CAMERA_HEIGHT = 400;
+	private int CAMERA_HEIGHT = 400;
+	
+	private Sprite ant;
 	
 	private GameGrid grid;
 	
-	public GameHolder(ScreenGame screenGame, int level)
+	public GameHolder(ScreenGame screen_game, int level)
 	{		
+		//Use the level parser to determine how many squares across and down our world has.
+		
+		
+		
+		//Create the camera using the found number of blocks above.
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 400);
+		camera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+		
+		ant = new Sprite(screen_game.assets.texture_ant);
+		ant.setBounds(0, 300, 50, 50);
 		
 		//Initialize the grid.
 		//grid = new GameGrid(origin_point, holder_size, level, block_textures);
@@ -35,6 +41,8 @@ public class GameHolder extends Holder
 	@Override
 	public void update(float delta)
 	{
+		ant.translateX(1f);
+		//camera.position.set(ant.getX(), ant.getY(), 0);
 		camera.update();
 	}
 
@@ -45,7 +53,7 @@ public class GameHolder extends Holder
 		batch.setProjectionMatrix(camera.combined);
 		
 		batch.begin();
-			
+			ant.draw(batch);
 		batch.end();
 	}
 }
--- core/src/com/leepresswood/antsoldiers/management/LevelParser.java
+++ core/src/com/leepresswood/antsoldiers/management/LevelParser.java
@@ -7,18 +7,24 @@ import com.badlogic.gdx.Gdx;
 
 public class LevelParser
 {
-	public static int[][] getGrid(int level)
+	public int height, width;
+	public int[][] grid;
+	
+	public LevelParser(int level)
 	{
+		//Read the text from the file.
 		Scanner scanner = new Scanner(Gdx.files.internal("levels/" + level + ".lvl").readString());
-		int height = scanner.nextInt();
-		int width = scanner.nextInt();		
-		int[][] grid = new int[height][width];
 		
+		//First two values are the width and height of the world (in blocks).
+		height = scanner.nextInt();
+		width = scanner.nextInt();		
+		
+		//Every integer that remains is the value for which block we're using.
+		grid = new int[height][width];
 		for(int j = 0; j < height; j++)
 			for(int i = 0; i < width; i++)
 				grid[j][i] = scanner.nextInt();
 		
 		scanner.close();		
-		return grid;
 	}
 }
