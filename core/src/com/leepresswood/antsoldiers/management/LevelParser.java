d1ac8ba169d9e2548f6858f8b7d97941328de4f2
Lee Presswood <pressedlee123@gmail.com>
2014-10-18 23:25:45 -0500

    No idea what was wrong with the file. Whatever.

--- core/src/com/leepresswood/antsoldiers/management/LevelParser.java
+++ core/src/com/leepresswood/antsoldiers/management/LevelParser.java
@@ -1,7 +1,6 @@
 //Read the passed in level's file and return a 2D array of the values.
 package com.leepresswood.antsoldiers.management;
 
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import com.badlogic.gdx.Gdx;
@@ -10,26 +9,17 @@ public class LevelParser
 {
 	public static int[][] getGrid(int level)
 	{
-		Scanner scanner;
-		int[][] grid = null;
-		try
-		{
-			scanner = new Scanner(Gdx.files.internal("levels/" + level + ".lvl").file());
-			int height = scanner.nextInt();
-			int width = scanner.nextInt();
-			grid = new int[height][width];
-			
-			for(int j = 0; j < height; j++)
-				for(int i = 0; i < width; i++)
-					grid[j][i] = scanner.nextInt();
-			
-			scanner.close();
-		}
-		catch(FileNotFoundException e)
-		{
-			System.out.println("Error: File not found.");
-		}
+		Scanner scanner = new Scanner(Gdx.files.internal("levels/" + level + ".lvl").readString());
 		
+		int height = scanner.nextInt();
+		int width = scanner.nextInt();		
+		int[][] grid = new int[height][width];
+		
+		for(int j = 0; j < height; j++)
+			for(int i = 0; i < width; i++)
+				grid[j][i] = scanner.nextInt();
+		
+		scanner.close();		
 		return grid;
 	}
 }
