8204b8eb1e75fc29a00378a89e89181315a152ce
Lee Presswood <pressedlee123@gmail.com>
2014-10-19 19:16:32 -0500

    Finished scrolling.

--- core/src/com/leepresswood/antsoldiers/holders/GameHolder.java
+++ core/src/com/leepresswood/antsoldiers/holders/GameHolder.java
@@ -3,7 +3,9 @@ package com.leepresswood.antsoldiers.holders;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.leepresswood.antsoldiers.gamegrid.GameGrid;
 import com.leepresswood.antsoldiers.management.LevelParser;
 import com.leepresswood.antsoldiers.screens.ScreenGame;
@@ -33,6 +35,8 @@ public class GameHolder extends Holder
 		
 		//Create the camera using the found number of blocks above.
 		camera = new OrthographicCamera(WORLD_VIEW, WORLD_VIEW * Gdx.graphics.getHeight() / Gdx.graphics.getWidth());
+		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+		camera.update();
 		
 		ant = new Sprite(screen_game.assets.texture_ant);
 		ant.setBounds(0, 0, 0.2f, 0.2f);
@@ -41,31 +45,38 @@ public class GameHolder extends Holder
 		//grid = new GameGrid(origin_point, holder_size, level, block_textures);
 	}
 
-	public void scroll()
-	{
+	public void scroll(float delta_x, float delta_y)
+	{//Do the scrolling.
+		camera.translate(delta_x, delta_y);
+		camera.update();
+	}
+	
+	public void scroll_stop()
+	{//Check camera boundaries and move if necessary.
+		float left = camera.viewportWidth / 2f + 1f;
+		float right = -camera.viewportWidth / 2f - 1f;
+		float top = left;
+		float down = right;
 		
+		//X
+		if(camera.position.x > left)
+			camera.position.set(left, camera.position.y, camera.position.z);
+		else if(camera.position.x < right)
+			camera.position.set(right, camera.position.y, camera.position.z);
+		
+		//Y
+		if(camera.position.y > top)
+			camera.position.set(camera.position.x, top, camera.position.z);
+		else if(camera.position.y < down)
+			camera.position.set(camera.position.x, down, camera.position.z);
+		
+		camera.update();
 	}
 
 	@Override
 	public void update(float delta)
 	{
-		//ant.translateX(0.1f);
-		//Do the scrolling.
-		if (Gdx.input.justTouched())
-		{
-			dragNew = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-			dragOld = dragNew;
-		}
-		if (Gdx.input.isTouched())
-		{
-			dragNew = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-			if (!dragNew.equals(dragOld))
-			{
-				camera.translate((dragOld.x - dragNew.x) * 0.0095f, 0); //Translate by subtracting the vectors
-				camera.update();
-				dragOld = dragNew; //Drag old becomes drag new.
-			}
-		}
+		//ant.translateX(0.1f);		
 		
 		//camera.position.set(ant.getX(), ant.getY(), 0);
 	}
--- core/src/com/leepresswood/antsoldiers/screens/ScreenGame.java
+++ core/src/com/leepresswood/antsoldiers/screens/ScreenGame.java
@@ -3,12 +3,15 @@ package com.leepresswood.antsoldiers.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
 import com.leepresswood.antsoldiers.GameAntSoldiers;
 import com.leepresswood.antsoldiers.holders.GUIHolder;
 import com.leepresswood.antsoldiers.holders.GameHolder;
 import com.leepresswood.antsoldiers.management.Assets;
 
-public class ScreenGame extends ScreenAdapter
+public class ScreenGame extends ScreenAdapter implements GestureListener
 {	
 	public GameAntSoldiers game;
 	public Assets assets;
@@ -23,6 +26,8 @@ public class ScreenGame extends ScreenAdapter
 				
 		gui_holder = new GUIHolder(this, level);
 		game_holder = new GameHolder(this, level);
+		
+		Gdx.input.setInputProcessor(new GestureDetector(this));
 	}
 
 	@Override
@@ -33,9 +38,61 @@ public class ScreenGame extends ScreenAdapter
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		game_holder.update(delta);
-		gui_holder.update(delta);
+		//gui_holder.update(delta);
 		
 		game_holder.render();
-		gui_holder.render();
+		//gui_holder.render();
+	}
+
+	//Touch logic below. Don't read unless you hate yourself.
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float x, float y)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY)
+	{//Move camera in the game world.
+		game_holder.scroll(-deltaX * 0.01f, deltaY * 0.01f);
+		return true;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button)
+	{//Make sure we're still bound to the game world.		
+		game_holder.scroll_stop();
+		return true;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
+			Vector2 pointer1, Vector2 pointer2)
+	{
+		return false;
 	}
 }
