a55350acbead52ed6d68a92d5d9a1add341f8233
Lee Presswood <pressedlee123@gmail.com>
2014-10-22 00:36:26 -0500

    Added repositioning method to ant.

--- core/src/com/leepresswood/antsoldiers/ants/Ant.java
+++ core/src/com/leepresswood/antsoldiers/ants/Ant.java
@@ -3,6 +3,7 @@ package com.leepresswood.antsoldiers.ants;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
+import com.leepresswood.antsoldiers.management.GameNumbers;
 
 public class Ant extends Sprite
 {
@@ -19,11 +20,31 @@ public class Ant extends Sprite
 		up = new Vector2();
 		down = new Vector2();
 		left = new Vector2();
-		right = new Vector2();
+		right = new Vector2();	
+		newPositions();
 	}
 
+	private void newPositions()
+	{//All vectors related to the middle of that side.		
+		up.x = position.x + GameNumbers.ANT_SIZE / 2f;
+		up.y = position.y + GameNumbers.ANT_SIZE;
+		
+		down.x = position.x + GameNumbers.ANT_SIZE / 2f;
+		down.y = position.y;
+		
+		left.x = position.x;
+		left.y = position.y + GameNumbers.ANT_SIZE / 2f;
+		
+		right.x = position.x + GameNumbers.ANT_SIZE;
+		right.y = position.y + GameNumbers.ANT_SIZE / 2f;
+	}
+	
 	public void update(float delta)
 	{//Don't change the position here. That will be changed by the blocks. This will change the walking animation and bounds of the ant.
+		//Animation.
 		
+		
+		//Positioning.
+		newPositions();
 	}
 }
