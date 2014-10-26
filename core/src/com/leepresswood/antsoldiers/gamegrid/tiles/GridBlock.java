b1dd8d317d81c1d93c7073cf301f380da35a855b
Lee Presswood <pressedlee123@gmail.com>
2014-10-26 12:18:29 -0500

    Added solid checker.

--- core/src/com/leepresswood/antsoldiers/gamegrid/tiles/GridBlock.java
+++ core/src/com/leepresswood/antsoldiers/gamegrid/tiles/GridBlock.java
@@ -3,6 +3,7 @@ package com.leepresswood.antsoldiers.gamegrid.tiles;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.leepresswood.antsoldiers.ants.Ant;
+import com.leepresswood.antsoldiers.management.GameNumbers;
 
 public abstract class GridBlock extends Sprite
 {
@@ -21,4 +22,30 @@ public abstract class GridBlock extends Sprite
 	 * this function will return the new location of the ant.
 	 */
 	public abstract void nextPosition(Ant ant, float antSpeed);
+	
+	public static boolean isSolid(int type, boolean is_left_of_block)
+	{//Call this in the turnaround logic. It determines if you can walk through this block or not.
+		switch(type)
+		{		
+			case GameNumbers.BLOCK_AIR:
+				return false;
+			case GameNumbers.BLOCK_GROUND_SOLID:
+				return true;
+			case GameNumbers.BLOCK_GROUND_SLOPED_RIGHT:
+				return is_left_of_block;
+			case GameNumbers.BLOCK_GROUND_SLOPED_LEFT:
+				return !is_left_of_block;
+			case GameNumbers.BLOCK_CEILING_SLOPED_RIGHT:
+				return is_left_of_block;
+			case GameNumbers.BLOCK_CEILING_SLOPED_LEFT:
+				return !is_left_of_block;
+			case GameNumbers.BLOCK_SPAWNER:
+				return false;
+			case GameNumbers.BLOCK_GOAL:
+				return false;
+			default:
+				System.out.println("Error: Type " + type + " is not defined for isSolid().");
+				return false;
+		}
+	}
 }
